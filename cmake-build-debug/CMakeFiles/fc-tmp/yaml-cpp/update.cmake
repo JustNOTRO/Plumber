21e5f7450cc22e15b8dabc012439ffd4bc83437c
Eilon H. <eilonhafzadi7311@gmail.com>
2025-12-01 22:48:00 +0200

    chore(server): delete cmake_build_debug

--- cmake-build-debug/CMakeFiles/fc-tmp/yaml-cpp/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for yaml-cpp")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/Users/eilon/CLionProjects/Plumber/cmake-build-debug/CMakeFiles/fc-tmp/yaml-cpp/yaml-cpp-gitupdate.cmake")
-
-endblock()
