a250dc07add93beb2fe60043706020154f34dd14
Eilon H. <eilonhafzadi7311@gmail.com>
2026-01-22 23:19:59 +0200

    chore: add HttpsServer.cpp server implementation

---
+++ src/server/http/HttpsServer.hpp
@@ -0,0 +1,75 @@
+//
+// Created by Eilon Hafzadi on 22/01/2026.
+//
+
+#pragma once
+
+#include "../Server.hpp"
+#include "spdlog/spdlog.h"
+
+class HttpsServer : public Server {
+public:
+    HttpsServer(const std::string &ip, const std::uint16_t port, const std::string &gitlab_instance, const std::string &certificate, const std::string &cert_key)
+    : Server(ip, port, gitlab_instance), server(certificate.c_str(), cert_key.c_str()) {}
+
+    bool bind_to_port(const std::string &ip, const std::uint16_t port) override {
+        return server.bind_to_port(ip, port);
+    }
+
+    bool listen_after_bind() override {
+        return server.listen_after_bind();
+    }
+
+    void listen(std::string &ip, std::uint16_t port) override {
+        if (!server.listen(ip, port)) {
+            spdlog::error("failed to listen on address {}:{}.", ip, port);
+            std::exit(1);
+        }
+    }
+
+    httplib::Server &Get(const std::string &pattern, const Handler handler) override {
+        return server.Get(pattern, handler);
+    }
+
+    httplib::Server &Post(const std::string &pattern, const Handler handler) override {
+        return server.Post(pattern, handler);
+    }
+
+    httplib::Server &Post(const std::string &pattern, HandlerWithContentReader handler) override {
+        return server.Post(pattern, handler);
+    }
+
+    httplib::Server &Put(const std::string &pattern, const Handler handler) override {
+        return server.Put(pattern, handler);
+    }
+
+    httplib::Server &Put(const std::string &pattern, HandlerWithContentReader handler) override {
+        return server.Put(pattern, handler);
+    }
+
+    httplib::Server &Patch(const std::string &pattern, const Handler handler) override {
+        return server.Patch(pattern, handler);
+    }
+
+    httplib::Server &Patch(const std::string &pattern, HandlerWithContentReader handler) override {
+        return server.Patch(pattern, handler);
+    }
+
+    httplib::Server &Delete(const std::string &pattern, const Handler handler) override {
+        return server.Delete(pattern, handler);
+    }
+
+    httplib::Server &Delete(const std::string &pattern, HandlerWithContentReader handler) override {
+        return server.Delete(pattern, handler);
+    }
+
+    httplib::Server &Options(const std::string &pattern, const Handler handler) override {
+        return server.Options(pattern, handler);
+    }
+
+protected:
+    void setup_gitlab_client() override;
+
+private:
+    httplib::SSLServer server;
+};
