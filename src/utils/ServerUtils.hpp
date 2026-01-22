04ece6d909e587d4e2cd2a6f315f620df24988f3
Eilon H. <eilonhafzadi7311@gmail.com>
2026-01-22 23:17:54 +0200

    chore: add ServerUtils.cpp

---
+++ src/utils/ServerUtils.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Eilon Hafzadi on 10/01/2026.
+//
+
+#pragma once
+
+#include <optional>
+#include <string>
+
+class ServerUtils {
+    public:
+        static std::string require_env(const char *name);
+
+        static std::uint16_t require_port(const char *name);
+
+        static std::string require_url_scheme(const char *name);
+
+        static int require_retry_amount(const char *name);
+
+        static std::optional<std::string> get_env(const char *name);
+};
